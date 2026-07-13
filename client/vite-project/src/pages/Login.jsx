26f9ed2246e353a10a2af82dde6fbb00996a1719
shivanshuGautam <harigautam772@gmail.com>
2026-07-13 23:48:33 +0530

    style: adjust margin in Login component for improved layout

--- client/vite-project/src/pages/Login.jsx
+++ client/vite-project/src/pages/Login.jsx
@@ -182,7 +182,7 @@ const Login = () => {
         </form>
 
         {/* Divider */}
-        <div className="relative mb-6">
+        <div className="relative mb-5">
           <div className="absolute inset-0 flex items-center">
             <div className="w-full border-t border-gray-300"></div>
           </div>
