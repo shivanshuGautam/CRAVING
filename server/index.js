e10e699ebd87aed1b7b86d5cddcf237a75206d50
shivanshuGautam <harigautam772@gmail.com>
2026-07-28 23:34:06 +0530

    change  index,js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,10 @@ import connectDB from "./src/config/dbConnection.config.js";
 import AuthRouter from "./src/router/auth.route.js";
 import PublicRouter from "./src/router/public.route.js";
 import CommonRouter from "./src/router/common.route.js";
+import AdminRouter from "./src/router/admin.route.js";
 import RestaurantRouter from "./src/router/restaurant.route.js";
+import CustomerRouter from "./src/router/customer.route.js";
+import RiderRouter from "./src/router/rider.route.js";
 import morgan from "morgan";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -20,7 +23,14 @@ app.use(morgan("dev"));
 app.use("/auth", AuthRouter);
 app.use("/public", PublicRouter);
 app.use("/common", CommonRouter);
+
+app.use("/admin", AdminRouter);
 app.use("/restaurant", RestaurantRouter);
+app.use("/customer", CustomerRouter);
+app.use("/rider", RiderRouter);
+
+
+
 //Default API
 app.get("/", (req, res) => {
   console.log("Default Get API Hit");
